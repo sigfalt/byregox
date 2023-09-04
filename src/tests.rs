c05493a1bcc8f91e555950a3490c63bb5e75baa5
sigfalt <1689082+sigfalt@users.noreply.github.com>
2023-09-04 05:11:41 +0000

    Add test for muscle memory

--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,11 @@
 pub mod tests {
-	use crate::types::{structs::*, *};
+
+	use std::error::Error;
+
+	use crate::types::{structs::{Craft, CrafterStats, CrafterLevels}, SimulationBuilder, actions};
 
 	#[test]
-	fn test_simulation_construction() -> Result<(), SimulationBuilderError> {
+	fn test_simulation_construction() -> Result<(), Box<dyn Error>> {
 		let recipe = Craft {
 			id: "1035".to_string(),
 			job: 14,
@@ -33,6 +36,40 @@ pub mod tests {
 		assert_eq!(result.simulation.steps[0].added_quality, 267);
 		assert_eq!(result.simulation.steps[1].added_quality, 293);
 		assert_eq!(result.simulation.steps[2].added_progression, 291);
+
+		Ok(())
+	}
+
+	#[test]
+	fn test_muscle_memory() -> Result<(), Box<dyn Error>> {
+		let recipe = Craft {
+			id: "1960".to_string(),
+			job: 14,
+			rlvl: 255,
+			durability: 80,
+			quality: 2790,
+			progress: 630,
+			lvl: 61,
+			hq: Some(true),
+			quick_synth: Some(true),
+			ingredients: vec![],
+			progress_divider: 81,
+			quality_divider: 58,
+			..Default::default()
+		};
+		let stats = generate_stats(90, 1208, 698, 187, false);
+		let sim = SimulationBuilder::default()
+			.recipe(recipe)
+			.actions(vec![
+				Box::new(actions::MuscleMemory),
+				Box::new(actions::BasicSynthesis)
+			])
+			.crafter_stats(stats)
+			.build()?;
+		let result = sim.run(true);
+		assert!(result.simulation.success.is_some_and(|x| x));
+		assert_eq!(result.simulation.steps[0].added_progression, 453);
+		assert_eq!(result.simulation.steps[1].added_progression, 362);
 
 		Ok(())
 	}
