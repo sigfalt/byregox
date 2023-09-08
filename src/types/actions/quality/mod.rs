69ac8a4252fed42fc2ce374edfa496a72e0e1418
sigfalt <1689082+sigfalt@users.noreply.github.com>
2023-09-08 04:27:57 +0000

    Add skeleton for quality actions

--- src/types/actions/quality/mod.rs
+++ src/types/actions/quality/mod.rs
@@ -1,2 +1,47 @@
+// level 5
 mod basic_touch;
 pub use basic_touch::*;
+
+// level 9
+mod hasty_touch;
+pub use hasty_touch::*;
+
+// level 18
+mod standard_touch;
+pub use standard_touch::*;
+
+// level 50
+mod byregots_blessing;
+pub use byregots_blessing::*;
+
+// level 53
+mod precise_touch;
+pub use precise_touch::*;
+
+// level 66
+mod prudent_touch;
+pub use prudent_touch::*;
+
+// level 68
+mod focused_touch;
+pub use focused_touch::*;
+
+// level 69
+mod reflect;
+pub use reflect::*;
+
+// level 71
+mod preparatory_touch;
+pub use preparatory_touch::*;
+
+// level 80
+mod trained_eye;
+pub use trained_eye::*;
+
+// level 84
+mod advanced_touch;
+pub use advanced_touch::*;
+
+// level 90
+mod trained_finesse;
+pub use trained_finesse::*;
