94c1c2e65050ecd1f0e78645cc5867e57a616b53
sigfalt <1689082+sigfalt@users.noreply.github.com>
2023-10-29 18:48:07 +0000

    Set simulation state to input step state if it exists

--- src/types/simulation.rs
+++ src/types/simulation.rs
@@ -90,63 +90,71 @@ impl Simulation {
 
 	pub fn run(mut self, linear: bool) -> SimulationResult {
 		self.last_possible_reclaim_step = None;
-		self.actions.clone().iter().for_each(|action| {
-			self.state = StepState::Normal; // TODO: this.step_states[index] || Normal;
-			let mut fail_cause: Option<&str> = None;
+		self.actions
+			.clone()
+			.iter()
+			.enumerate()
+			.for_each(|(i, action)| {
+				self.state = self
+					.step_states
+					.get(i)
+					.map_or_else(|| StepState::Normal, |s| s.clone());
+				let mut fail_cause: Option<&str> = None;
 
-			let can_use_action = action.can_be_used(&self);
-			if !can_use_action {
-				fail_cause = action.get_fail_cause(&self);
-			}
-			let has_enough_cp = action.get_base_cp_cost(&self) <= self.available_cp;
-			if !has_enough_cp {
-				fail_cause = Some("Not enough CP");
-			}
-			// we can use the action
-			let result = if self.success.is_none()
+				let can_use_action = action.can_be_used(&self);
+				if !can_use_action {
+					fail_cause = action.get_fail_cause(&self);
+				}
+				let has_enough_cp = action.get_base_cp_cost(&self) <= self.available_cp;
+				if !has_enough_cp {
+					fail_cause = Some("Not enough CP");
+				}
+				// we can use the action
+				let result = if self.success.is_none()
                     && has_enough_cp
                     // TODO: && self.steps.len() < max_turns
                     && can_use_action
-			{
-				self.run_action(action, linear)
-			} else {
-				ActionResult {
-					action: action.clone(),
-					success: None,
-					fail_cause: fail_cause.map(|x| x.to_string()),
-					added_progression: 0,
-					added_quality: 0,
-					cp_difference: 0,
-					solidity_difference: 0,
-					skipped: true,
-					combo: None,
-					state: self.state,
-				}
-			};
+				{
+					self.run_action(action, linear)
+				} else {
+					ActionResult {
+						action: action.clone(),
+						success: None,
+						fail_cause: fail_cause.map(|x| x.to_string()),
+						added_progression: 0,
+						added_quality: 0,
+						cp_difference: 0,
+						solidity_difference: 0,
+						skipped: true,
+						combo: None,
+						state: self.state,
+					}
+				};
 
-			// TODO: if self.steps.len() < max_turns
-			if self.steps.len() < usize::MAX {
-				let quality_before = self.quality;
-				let progression_before = self.progression;
-				let durability_before = self.durability;
-				let cp_before = self.available_cp;
-				let skip_ticks_on_fail = !result.success.unwrap_or(false) && action.skip_on_fail();
-				if self.success.is_none() && !action.skips_buff_ticks() && !skip_ticks_on_fail {
-					self.tick_buffs(action.as_ref());
+				// TODO: if self.steps.len() < max_turns
+				if self.steps.len() < usize::MAX {
+					let quality_before = self.quality;
+					let progression_before = self.progression;
+					let durability_before = self.durability;
+					let cp_before = self.available_cp;
+					let skip_ticks_on_fail =
+						!result.success.unwrap_or(false) && action.skip_on_fail();
+					if self.success.is_none() && !action.skips_buff_ticks() && !skip_ticks_on_fail {
+						self.tick_buffs(action.as_ref());
+					}
+					// TODO: result.after_buff_tick = {
+					//     added_progression: self.progression - progression_before,
+					// };
 				}
-				// TODO: result.after_buff_tick = {
-				//     added_progression: self.progression - progression_before,
-				// };
-			}
 
-			if !linear
-				&& action.get_enum() != CraftingActionEnum::FinalAppraisal
-				&& action.get_enum() != CraftingActionEnum::RemoveFinalAppraisal
-			{
-				self.tick_state();
-			}
-			self.steps.push(result);
-		});
+				if !linear
+					&& action.get_enum() != CraftingActionEnum::FinalAppraisal
+					&& action.get_enum() != CraftingActionEnum::RemoveFinalAppraisal
+				{
+					self.tick_state();
+				}
+				self.steps.push(result);
+			});
 
 		let failed_action = self
 			.steps
