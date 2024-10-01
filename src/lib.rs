23a9fc170f20f5b1df998424e419e24f34e0fca7
sigfalt <1689082+sigfalt@users.noreply.github.com>
2024-10-01 06:48:46 +0000

    docs: initial basic crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,42 @@
+
+//! # FFXIV Tangocraft
+//!
+//! A simulation library for the crafting system of FINAL FANTASY XIV.
+//! Heavily inspired by (mostly just a Rust rewrite of) the simulator created by the
+//! ffxiv-teamcraft group, which can be found [here](https://github.com/ffxiv-teamcraft/simulator).
+//!
+//! ## Basic Usage
+//! ```rust
+//! use ffxiv_tangocraft::types::{actions, Simulation};
+//! use ffxiv_tangocraft::types::structs::{Craft, CrafterLevels, CrafterStats, CraftingLevel};
+//!
+//! let recipe = Craft {
+//!     ..Default::default()
+//! };
+//!
+//! let stats = CrafterStats {
+//!     craftsmanship: 4936,
+//!     control: 4943,
+//!     cp: 627,
+//!     level: CraftingLevel::max(),
+//!     levels: CrafterLevels::max(),
+//!     ..Default::default()
+//! };
+//!
+//! let sim = Simulation::builder()
+//!     .recipe(recipe)
+//!     .actions(vec![
+//!         actions::Reflect.into(),
+//!         actions::BasicTouch.into(),
+//!         actions::ByregotsBlessing.into(),
+//!         actions::CarefulSynthesis.into(),
+//!     ])
+//!     .crafter_stats(stats)
+//!     .build();
+//!
+//! let result = sim.start().safe(true).run();
+//! ```
+
 #![forbid(unsafe_code)]
 pub mod types;
 
